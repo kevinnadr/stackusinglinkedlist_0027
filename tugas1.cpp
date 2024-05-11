d60413aec9d13b0738609a21a92f70f13a23d569
kevinnadr <kevin19305.ib@gmail.com>
2024-05-11 10:42:33 +0700

    Allocate memory for the new node

--- tugas1.cpp
+++ tugas1.cpp
@@ -1,26 +1,28 @@
 #include <iostream>
 using namespace std;
 
-// Node class representing a single node in the linked list
+//Node class representing a single node in the linked list
+
 class Node {
 public:
-    int data;
-    Node* next;
+	int data;
+	Node* next;
 
-    Node() {
-        next = NULL;
-    }
+	Node() {
+		next = NULL;
+	}
 };
 
 // Stack class
 class Stack {
 private:
-    Node* top // pointer to the top node of the stack
+	Node* top; // Pointer to the top node of the stack
 
 public:
-    Stack() {
-        top = NULL; // Intialize the stack with a null top pointer
-    }
+	Stack() {
+		top = NULL; //Initialize the stack with a null toppointer
+	}
 
-    // Push operation: Insert an element onto the top of the stack
-}
+	// Push operation: Insert an element onto the top of the stack
+	int push(int value) {
+		Node* newNode = new Node(); //1.Allocate memory for the new node
