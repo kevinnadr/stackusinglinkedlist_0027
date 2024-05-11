18085c36ac0b7c18a864f40d8239027b036c3cfa
kevinnadr <kevin19305.ib@gmail.com>
2024-05-11 10:48:59 +0700

    Update the top pointer to the next node

--- tugas1.cpp
+++ tugas1.cpp
@@ -34,3 +34,11 @@ public:
 	}
 
 	// Pop Operation: Remove the topmost element from the stack
+	void pop() {
+		if (isEmpty()) {
+			cout << "Stack is empty." << endl;
+		}
+		cout << "Popped value: " << top->data << endl;
+		top = top->next; // Update the top pointer to the next node
+
+	}
